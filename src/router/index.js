22cd0aef7a8b8732298be6516fbc4faa886fd799
Dan <dcox38@gmailL.com>
2020-08-26 20:49:00 -0500

    vur frontend routes working

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, 
+  { 
+    path: "/signup", 
+    name: "signup", 
+    component: Signup 
   },
   {
     path: '/about',
@@ -18,12 +25,12 @@ Vue.use(VueRouter)
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
